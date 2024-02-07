4a97d74478887bfcd62dd1e2e8226da516e3c25f
DipRabadiya27 <diprabadiya@gmail.com>
2024-02-07 15:01:24 +0530

    Delete Old ApiGateway

---
+++ UserService/src/main/java/com/user/service/UserService/Config/interceptor/FeignClientInterceptor.java
@@ -0,0 +1,13 @@
+package com.user.service.UserService.Config.interceptor;
+
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        
+    }
+}
